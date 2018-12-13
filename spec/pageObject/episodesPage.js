947e19e78296524f8b5f5645619d76903255a182
Volha.Bondarava <vol.bondarava@gmail.com>
2018-12-13 18:32:57 +0300

    Add swipe action

--- spec/pageObject/episodesPage.js
+++ spec/pageObject/episodesPage.js
@@ -1,24 +1,21 @@
 let wd = require('wd')
+let actions = require("../helpers/actions")
 let navDrawer = require('.//navigationDrawer')
 
 class episodesPage extends navDrawer {
   constructor (driver) {
+    wd.addPromiseChainMethod('swipe', actions.swipe)
     super(driver)
   }
 
   async getRecommendedEpisodes () {
     await (new wd.TouchAction(this.driver)).tap({x: 540, y: 960}).perform()
-    await (new wd.TouchAction(this.driver))
-      .press({x: 639, y: 150})
-      .moveTo({x: 639, y: 1647})
-      .release()
-      .perform ()
-    await (new wd.TouchAction(this.driver))
-      .press({x: 639, y: 1647})
-      .moveTo({x: 639, y: 150})
-      .release()
-      .perform ()
-    await this.driver.sleep(3000)
+
+    return this.driver.swipe({
+      startX: 610, startY: 1647,
+      endX: 640, endY: 150,
+      duration: 800
+    })
   }
 }
 
